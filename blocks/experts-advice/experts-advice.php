2cd50f4ea7c3ca81a277a8e5c42eafc362ba4bc1
petschoice-ederlyn <ederlyn.batac@petschoice.co.uk>
2024-07-15 15:46:02 +0800

    experts-advice block

--- blocks/experts-advice/experts-advice.php
+++ blocks/experts-advice/experts-advice.php
@@ -21,7 +21,38 @@ if ( isset($block['data']['preview_image_experts_advice']) ) :
 else :
 ?>
     <div <?php echo $wrapper_attributes; ?>>
-        
+        <?php if ( have_rows( 'vk_ea_top_content' ) ) : ?>
+        <?php while ( have_rows( 'vk_ea_top_content' ) ) : the_row(); ?>
+            <div class="vetskitchen-experts-advice__top">
+                <?php echo get_sub_field( 'heading' ) ? '<h2 class="heading">'.get_sub_field( 'heading' ).'</h2>' : ''; ?>
+                <?php echo wp_get_attachment_image( get_sub_field( 'image' ), 'large' ); ?>
+                <?php echo get_sub_field( 'text' ) ? '<div class="text">'.get_sub_field( 'text' ).'</div>' : ''; ?>
+            </div>
+        <?php endwhile; ?>
+        <?php endif; ?>
+        <?php if ( have_rows( 'vk_ea_main_content' ) ) : ?>
+        <?php while ( have_rows( 'vk_ea_main_content' ) ) : the_row(); ?>
+            <div class="vetskitchen-experts-advice__main">
+                <div class="left-content">
+                    <?php echo get_sub_field( 'heading' ) ? '<h2 class="heading">'.get_sub_field( 'heading' ).'</h2>' : ''; ?>
+                    <?php echo get_sub_field( 'content' ) ? '<div class="content">'.get_sub_field( 'content' ).'</div>' : ''; ?>
+                    <?php 
+                    $link = get_sub_field('button');
+                    if( $link ): 
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                        ?>
+                        <a href="<?php echo esc_url( $link_url ); ?>" class="btn-orange" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                    <?php endif; ?>
+                </div>
+                <div class="right-content">
+                    <?php echo wp_get_attachment_image( get_sub_field( 'image_desktop' ), 'large', "", ["class" => "right-content__image d-none d-md-block"] ); ?>
+                    <?php echo wp_get_attachment_image( get_sub_field( 'image_mobile' ), 'large', "", ["class" => "right-content__image d-md-none"] ); ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 <?php
 endif;    
