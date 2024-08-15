95163a52f4878618f84b68ca6fc9c64881eaf223
petschoice-ederlyn <ederlyn.batac@petschoice.co.uk>
2024-08-15 10:03:19 +0800

    changed X icon

--- footer.php
+++ footer.php
@@ -130,7 +130,9 @@
                                             <a href="<?php echo $fb; ?>" class="icon-g-64" target="_blank"></a>
                                         <?php endif; ?>
                                         <?php if ( $twitter = get_field( 'vk_twitter_url', 'option' ) ) : ?>
-                                            <a href="<?php echo $twitter; ?>" class="icon-h-58" target="_blank"></a>
+                                            <a href="<?php echo $twitter; ?>" target="_blank">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16px" height="16px"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" fill="#c3c3c3"></path></svg>
+                                            </a>
                                         <?php endif; ?>
                                         <?php if ( $ig = get_field( 'vk_instagram_url', 'option' ) ) : ?>
                                             <a href="<?php echo $ig; ?>" class="icon-g-67" target="_blank"></a>
