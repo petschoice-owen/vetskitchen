caa8c803afe83a78e093e77438cd8689a3b31542
petschoice-ederlyn <ederlyn.batac@petschoice.co.uk>
2024-08-27 20:43:32 +0800

    meta pixel code

--- header.php
+++ header.php
@@ -17,6 +17,23 @@
 		<link href="https://fonts.googleapis.com/css?family=Signika:100,200,300,400,500,600,700,800,900" rel="stylesheet" defer>
 		<link href="https://fonts.googleapis.com/css?family=Hind:100,200,300,400,500,600,700,800,900" rel="stylesheet" defer>
 		<?php wp_head(); ?>
+		<!-- Meta Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '2328537173854330');
+		fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src=https://www.facebook.com/tr?id=2328537173854330&ev=PageView&noscript=1
+		/></noscript>
+		<!-- End Meta Pixel Code -->
 	</head>
 	<body <?php body_class();?>>
 		<?php
